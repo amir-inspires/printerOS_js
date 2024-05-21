9f846f0edfc5dea4082ded88b2982af1cbdb2bbc
Amir Moadad <71446261+amir-inspires@users.noreply.github.com>
2024-05-21 03:00:45 +0300

    updated code

--- printer.js
+++ printer.js
@@ -2,13 +2,32 @@ const readline = require('readline');
 const chalk = require('chalk');
 
 class Process {
-  constructor(id, name, priority, owner, estimatedTime, status = 1) {
-    this.id = id;
+  id = 0;
+  constructor(name, priority, owner, estimatedTime, status = 1) {
+    this.id += 1;
     this.name = name;
     this.priority = priority;
     this.owner = owner;
     this.estimatedTime = estimatedTime;
     this.status = status;
+  }
+
+  printStatus(process){
+    switch (process.status){
+      case 0:
+        return 'blocked';
+      case 1:
+        return 'ready';
+      case 3:
+        return 'printing';
+      case 4:
+        return 'done';
+    }
+      
+  }
+
+  toString() {
+    return `${this.id}. ${this.name} (p: ${this.priority}, o: ${this.owner}, e: ${this.estimatedTime}, s: ${this.printStatus(this)})`;
   }
 }
 
@@ -49,13 +68,12 @@ class PrinterOS {
         case 'help':
           this.help();
           break;
-        case 'view':
-          this.viewProcess(args[0]);
+        case 'init':
+          this.init();
           break;
         case 'exit':
           console.log('Exiting...');
           process.exit();
-          break;
         default:
           console.log(chalk.red('Invalid command. Use "help" for a list of available commands.'));
       }
@@ -71,7 +89,8 @@ class PrinterOS {
 
   addProcess(process) {
     this.readyQueue.push(process);
-    console.log(chalk.green('Process added to the ready queue.'));
+    this.readyQueue.sort((a, b) => this.comparator(a.priority, b.priority));
+    console.log(chalk.yellow('Process added to the ready queue.'));
     this.printQueues();
   }
 
@@ -89,9 +108,8 @@ class PrinterOS {
       return;
     }
   
-    const process = new Process(id, name, parseInt(priority), new User('Console User'), parseInt(estimatedTime));
+    const process = new Process(name, parseInt(priority), 'Console User', parseInt(estimatedTime));
     this.addProcess(process);
-    this.readyQueue.sort((a, b) => this.comparator(a.priority, b.priority));
   }
 
   block() {
@@ -100,6 +118,7 @@ class PrinterOS {
       this.blockedQueue.push(this.executingProcess);
       this.executingProcess = null;
       console.log(chalk.yellow('Process blocked.'));
+      this.executeProcess();
       this.printQueues();
     } else {
       console.log(chalk.red('No process is currently executing.'));
@@ -112,7 +131,8 @@ class PrinterOS {
       while (this.blockedQueue.length > 0) {
         this.readyQueue.push(this.blockedQueue.shift());
       }
-      console.log(chalk.green('Process unblocked.'));
+      this.readyQueue.sort((a, b) => this.comparator(a.priority, b.priority));
+      console.log(chalk.yellow('Process unblocked.'));
       this.printQueues();
     } else {
       console.log(chalk.red('No blocked processes.'));
@@ -122,7 +142,8 @@ class PrinterOS {
   executeProcess() {
     if (this.readyQueue.length > 0) {
       this.executingProcess = this.readyQueue.shift();
-      console.log(chalk.blue('Process executing: ' + this.executingProcess.name));
+      this.executingProcess.status = 3;
+      console.log(chalk.yellow(`Process executing: ${this.executingProcess.name}`));
       this.printQueues();
     } else {
       console.log(chalk.red('No processes to execute.'));
@@ -131,8 +152,10 @@ class PrinterOS {
 
   doneExecuting() {
     if (this.executingProcess) {
-      console.log(chalk.green('Process finished: ' + this.executingProcess.name));
+      this.executingProcess.status = 4;
+      console.log(chalk.green(`Process finished: ${this.executingProcess.name}`));
       this.executingProcess = null;
+      this.executeProcess();
       this.printQueues();
     } else {
       console.log(chalk.red('No process is currently executing.'));
@@ -146,30 +169,51 @@ class PrinterOS {
     console.log('  unblock');
     console.log('  execute');
     console.log('  done');
-    console.log('  view <processId>');
+    console.log('  init');
     console.log('  help');
     console.log('  exit');
   }
 
-  viewProcess(processId) {
-    const process = this.readyQueue.find(p => p.id === parseInt(processId)) || this.executingProcess || this.blockedQueue.find(p => p.id === parseInt(processId));
-
-    if (process) {
-      console.log(chalk.green('Process ID: ' + process.id));
-      console.log('Name: ' + process.name);
-      console.log('Priority: ' + process.priority);
-      console.log('Owner: ' + process.owner.name);
-      console.log('Estimated Time: ' + process.estimatedTime);
-      console.log('Status: ' + (process.status === 1 ? 'Executing' : 'Blocked'));
-    } else {
-      console.log(chalk.red('Process not found.'));
-    }
+  init(){
+    const p1 = new Process('sample1', 1, 'user', 3);
+    const p2 = new Process('sample2', 2, 'user', 5);
+    const p3 = new Process('sample3', 3, 'user', 7);
+    const p4 = new Process('sample4', 2, 'user', 8);
+    const p5 = new Process('sample5', 1, 'user', 10);
+    const p6 = new Process('sample6', 3, 'user', 6);
+    
+    this.addProcess(p1);
+    this.addProcess(p2);
+    this.addProcess(p3);
+    this.executeProcess();
+    this.doneExecuting();
+    this.block();
+    this.addProcess(p4);
+    this.doneExecuting();
+    this.addProcess(p5);
+    this.block();
+    this.addProcess(p6);
+    this.unblock();
+    this.doneExecuting();
+    this.doneExecuting();
+    this.doneExecuting();
+    this.doneExecuting();
+    
+    console.log('Exiting...');
+    process.exit();
   }
 
   printQueues() {
-    console.log(chalk.blue('Ready Queue: ' + this.readyQueue.map(p => p.id).join(', ')));
-    console.log(chalk.blue('Blocked Queue: ' + this.blockedQueue.map(p => p.id).join(', ')));
-    console.log(chalk.blue('Executing Process: ' + (this.executingProcess ? this.executingProcess.id : 'None')));
+    console.log(chalk.bgBlue.black('Ready Queue:'));
+    this.readyQueue.forEach((process) => {
+      console.log(chalk.blue(process.toString()));
+    });
+    console.log(chalk.bgRed.black('\nBlocked Queue:'));
+    this.blockedQueue.forEach((process) => {
+      console.log(chalk.red(`${process.id}. ${process.name} (p: ${process.priority}, o: ${process.owner}, e: ${process.estimatedTime}, s: blocked)`));
+    });
+    console.log(chalk.bgGreen.black('\nExecuting Process:'));
+    console.log(chalk.green(this.executingProcess ? this.executingProcess.toString() : 'None') + '\n');
   }
 
   comparator(a, b) {
@@ -180,5 +224,5 @@ class PrinterOS {
   }
 }
 
-console.log("Welcome to Printer OS. Type help to check available commands.");
+console.log("Welcome to Printer OS. Type 'help' to check available commands.");
 const printerOS = new PrinterOS();
